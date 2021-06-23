ee049569bbae7f5b6ebc44dd3d5051b88a2c4e23
bhavya-boppana <60006124+bhavya-boppana@users.noreply.github.com>
2021-06-23 22:02:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ Created on Mon Jun 21 16:31:37 2021
 @author: bhavya boppana
 """
 print("started")
+import sys
 py -m ensurepip --default-pip
 pip install --upgrade tableau-api-lib
 from tableau_api_lib import TableauServerConnection
